bf164a3016a55c6d8a92eaa0f499b8f6bbbfb765
SpearedLinux <tsukinozomi@yahoo.com>
2018-05-02 16:13:25 +0000

    updating BasketCalculator's constructor

--- src/com/bakery/demo/BasketCalculator.java
+++ src/com/bakery/demo/BasketCalculator.java
@@ -1,5 +1,14 @@
 package com.bakery.demo;
 
 public class BasketCalculator {
-
+	
+	double total;
+	
+	BasketCalculator(double setBreadAmount,
+			double setRollsAmount, 
+			double priceBread, 
+			double priceRoll) {
+		total = (setBreadAmount * priceBread) +
+				(setRollsAmount * priceRoll);
+	}
 }
