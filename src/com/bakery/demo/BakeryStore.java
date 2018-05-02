5edf2e53e125987d5434b384363fbd1d56547057
SpearedLinux <tsukinozomi@yahoo.com>
2018-05-02 16:28:07 +0000

    instantiated BasketCalculator in main

--- src/com/bakery/demo/BakeryStore.java
+++ src/com/bakery/demo/BakeryStore.java
@@ -5,6 +5,8 @@ public class BakeryStore {
 		BakeryPrices prices;
 		double price1 = 0;
 		double price2 = 0;
+		double amountBread = 0;
+		double amountRolls = 0;
 		
 		System.out.println("Welcome to the store");
 		System.out.println("Pick your product");
@@ -12,10 +14,16 @@ public class BakeryStore {
 		if(arguments.length > 1) {
 			price1 = Double.parseDouble(arguments[0]);
 			price2 = Double.parseDouble(arguments[1]);
+			amountBread = Double.parseDouble(arguments[2]);
+			amountRolls = Double.parseDouble(arguments[3]);
+			
 		}
 			prices = new BakeryPrices(
 					price1, price2);
 		
 		prices.ShowPrices();
+		
+		BasketCalculator basket = new BasketCalculator(price1, price2,
+				amountBread, amountRolls);
 	}
 }
