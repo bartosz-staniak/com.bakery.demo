26a182f0e191ad9cb4f9d7a34372ec827e7da623
SpearedLinux <tsukinozomi@yahoo.com>
2018-05-02 12:46:21 +0000

    payReceipt method added

--- src/com/bakery/demo/CustomerWallet.java
+++ src/com/bakery/demo/CustomerWallet.java
@@ -10,4 +10,16 @@ public class CustomerWallet {
 	public double showContents() {
 		return customerWallet;
 	}
+	
+	public void payReceipt(double outMoney) {
+		if (outMoney < customerWallet) {
+			customerWallet -= outMoney;
+			System.out.println("Your current wallet"
+					+ "amount is: " + customerWallet);
+		}
+
+		else
+			System.out.println("Not enough money"
+					+ "in the wallet!");
+	}
 }
