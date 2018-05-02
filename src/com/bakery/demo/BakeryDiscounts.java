479cd7692f30e17bafbb450b58fd35333babe073
Bartosz Staniak <bartosz.staniak@gmail.com>
2018-05-03 00:28:00 +0200

    created BakeryDiscounts constructor

--- src/com/bakery/demo/BakeryDiscounts.java
+++ src/com/bakery/demo/BakeryDiscounts.java
@@ -1,5 +1,17 @@
 package com.bakery.demo;
 
 public class BakeryDiscounts {
-
+	double discount;
+	
+	BakeryDiscounts(double inPriceTotal) {
+		if(inPriceTotal < 10)
+			discount = 1;
+		else if(inPriceTotal < 50)
+			discount = 0.99;
+		else if(inPriceTotal < 100)
+			discount = 0.95;
+		else
+			discount = 0.9;
+			
+	}
 }
