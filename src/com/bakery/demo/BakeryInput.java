d0b4ddf3b264e3483c9c12ae39a6ec745792f0f4
Bartosz Staniak <bartosz.staniak@gmail.com>
2018-05-04 07:28:08 +0200

    implemented try/catch blocks for BufferedInputStream inside readValue method in BakeryInput class

--- src/com/bakery/demo/BakeryInput.java
+++ src/com/bakery/demo/BakeryInput.java
@@ -7,6 +7,14 @@ public class BakeryInput {
 	public static Double readValue() {
 		Double value = 0.0;
 		
+		try (BufferedInputStream buffer = new
+			BufferedInputStream(System.in)) {
+			// implement logic here
+		} catch (IOException except) {
+			System.out.println("Exception: " + except.getMessage());
+			return 0.0;
+		}
+		
 		return value;
 	}
 	
