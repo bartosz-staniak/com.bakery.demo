01ba6e72592256b7fbab283eeaae426a432c80e9
Bartosz Staniak <bartosz.staniak@gmail.com>
2018-05-05 02:08:18 +0200

    Double types changed to double. Code shortened for more clarity. Printed numbers are ASCII codes. Comment changed accordingly.

--- src/com/bakery/demo/BakeryInput.java
+++ src/com/bakery/demo/BakeryInput.java
@@ -1,35 +1,32 @@
-package com.bakery.demo;
-
-import java.io.*;
-
-public class BakeryInput {
-
-	public static Double readValue() {
-		Double value = 0.0;
-		
-		try (BufferedInputStream buffer = new
-			BufferedInputStream(System.in)) {
-				
-				value = (double) buffer.read();
-				
-		} catch (IOException except) {
-			System.out.println("Exception: " + except.getMessage());
-			return 0.0;
-		}
-		
-		return value;
-	}
-	
+ package com.bakery.demo;
+ 
+ import java.io.*;
+ 
+ public class BakeryInput {
+ 
+ 	public static double readValue() {
+ 		double value = 0.0;
+ 		
+ 		try (BufferedInputStream buffer = new
+ 			BufferedInputStream(System.in)) {
+ 				
+ 				return value = buffer.read();
+ 				
+ 		} catch (IOException except) {
+ 			System.out.println("Exception: " + except.getMessage());
+ 			return 0.0;
+ 		}
+ 		
+ 	}
+ 	
 	public static void main(String[] arguments) {
-		BakeryInput inputTest = new BakeryInput();
 		
 		System.out.println("Enter a number:");
 		System.out.println("The number you have entered is: " 
-				+ inputTest.readValue());
+				+ BakeryInput.readValue());
 		
-		// The logic is broken. The program converts different
-		// input signs to numbers instead of returning the numbers
-		// that were entered.
+		// The logic is broken. The program returns ASCII codes
+		// of a given input sign.
 	}
 	
-}
+ }
