1e587c7acb70575bd6c7ce7554b2ecd7af65565c
Bartosz Staniak <bartosz.staniak@gmail.com>
2018-05-08 00:14:06 +0200

    code not working. See line 16 of BakeryDB class for more info

--- src/com/bakery/demo/BakeryDB.java
+++ src/com/bakery/demo/BakeryDB.java
@@ -5,22 +5,33 @@ import java.sql.*;
 public class BakeryDB {
 	
 	public void Create() {
-	String driver = "org.apache.derby.jdbc."
-			+ "ClientDriver";
-	String dbaseURL = "jdbc:derby://localhost:1527/BakeryDB;create=true";
+		
+		String dbaseURL = "jdbc:derby://localhost:1527/BakeryDB;create=true";
 	
-	try(
-		Connection conn = DriverManager.getConnection
-			(dbaseURL)
-		) {
+		try(
+				Connection conn = DriverManager.getConnection
+					(dbaseURL)
+				) {
+
+			Class.forName("org.apache.derby.jdbc.ClientDriver"); // Does not work. Seems like the eclipse is not
+			// configured properly.
+			// java.sql.SQLException: No suitable driver found for jdbc:derby://localhost:1527/BakeryDB;create=true
+			
+			PreparedStatement ps = conn.prepareStatement("CREATE TABLE Prices "
+					+ "(ProductName CHAR(50),Price DECIMAL(5, 2))");
+			ps.executeQuery();
 		
-		PreparedStatement ps = conn.prepareStatement("CREATE TABLE Prices "
-				+ "(ProductName CHAR(50),Price DECIMAL(5, 2))");
-		ps.executeQuery();
-		
-	} catch (Exception e) {
-		e.printStackTrace();
+			} catch (SQLException se) {
+				se.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
-}
+	
+	public static void main(String[] arguments) {
+		
+		BakeryDB db = new BakeryDB();
+		db.Create();
+	}
 }
 
