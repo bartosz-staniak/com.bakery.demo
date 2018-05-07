a1e1585bc50365b405bade82061744731eb67be2
Bartosz Staniak <bartosz.staniak@gmail.com>
2018-05-07 22:30:11 +0200

    add a method in BakeryDB that creates a table

--- src/com/bakery/demo/BakeryDB.java
+++ src/com/bakery/demo/BakeryDB.java
@@ -3,5 +3,24 @@ package com.bakery.demo;
 import java.sql.*;
 
 public class BakeryDB {
-
+	
+	public void Create() {
+	String driver = "org.apache.derby.jdbc."
+			+ "ClientDriver";
+	String dbaseURL = "jdbc:derby://localhost:1527/BakeryDB;create=true";
+	
+	try(
+		Connection conn = DriverManager.getConnection
+			(dbaseURL)
+		) {
+		
+		PreparedStatement ps = conn.prepareStatement("CREATE TABLE Prices "
+				+ "(ProductName CHAR(50),Price DECIMAL(5, 2))");
+		ps.executeQuery();
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
 }
+}
+
