7cbe441d66b3430a5bc39db9a7ad0d83c79c20d7
Bartosz Staniak <bartosz.staniak@gmail.com>
2018-05-11 09:53:58 +0200

    infinite loop in BakeryShowProducts.chooseMethod fixed

--- src/com/bakery/demo/BakeryShowProducts.java
+++ src/com/bakery/demo/BakeryShowProducts.java
@@ -5,37 +5,43 @@ public class BakeryShowProducts {
 		String[] allergens = {"Eggs", "Milk", "Tree Nuts", "Corn", "Maize", "Wheat"};
 		String allergenName = "";
 		String answer = "";
+		String exit = "exit";
 		BakeryInput stringInput = new BakeryInput();
 		BakeryDB db = new BakeryDB();
 		
-		System.out.println("Would you like to browse"
-				+ "the products by alergens?\n"
-				+ "If yes press 'y' key and press enter.");
-		answer = stringInput.readString();
-		System.out.println(answer);
-		if ( answer.equals("y") ) {
-			System.out.println("Type the name of the allergen "
-					+ "and press enter:");
-			allergenName = stringInput.readString();
-			boolean exists = false;
-			int i = 0;
-			while (exists == false && i <= allergens.length) {
-				if ( (i < allergens.length) && (allergenName.equals(allergens[i])) ) {
-					exists = true;
-					db.filterByAllergen(allergenName);
-					return;
+		while (true) {
+			System.out.println("Would you like to browse"
+					+ "the products by alergens?\n"
+					+ "If yes press 'y' key and press enter.");
+			answer = stringInput.readString();
+			System.out.println(answer);
+			if ( answer.equals("y") ) {
+				System.out.println("Type the name of the allergen "
+						+ "and press enter:");
+				allergenName = stringInput.readString();
+				boolean exists = false;
+				int i = 0;
+				while (exists == false && i <= allergens.length) { // enters infinite loop. Needs an exit condition.
+					if ( (i < allergens.length) && (allergenName.equals(allergens[i])) ) {
+						exists = true;
+						db.filterByAllergen(allergenName);
+						return;
+					}
+					else if ( (exists == false) && (i >= allergens.length) ) {
+						System.out.println("Unable to verify presence"
+								+ "of the specified allergen\n."
+								+ "We know of the following allergnes"
+								+ "present in our products: Eggs, Milk, "
+								+ "Tree Nuts, Corn, Maize, Wheat");
+						break;
+					}
+					else
+						i++;
 				}
-				else if ( (exists == false) && (i >= allergens.length) ) {
-					System.out.println("Unable to verify presence"
-							+ "of the specified allergen\n."
-							+ "We know of the following allergnes"
-							+ "present in our products: Eggs, Milk, "
-							+ "Tree Nuts, Corn, Maize, Wheat");
-				}
-				else
-				i++;
-			}
 			
+			}
+			else
+				break;
 		}
 	}
 }
