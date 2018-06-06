a7cc4e2f76248f950eade42daa742d072588e019
Bartosz Staniak <bartosz.staniak@gmail.com>
2018-06-06 04:24:41 +0200

    added a comment to an unused class in order to emphasize usage of 'this' for future reference

--- src/com/bakery/demo/BakeryPrices.java
+++ src/com/bakery/demo/BakeryPrices.java
@@ -5,6 +5,10 @@ public class BakeryPrices {
 	double breadRoll = 0;
 	
 	BakeryPrices(double breadPrice, double rollPrice) {
+		
+		// use of "this" in this case seems to be unnecessary
+		// as it would be required if argument's name was
+		// the same as the variable name
 		this.bread = breadPrice;
 		this.breadRoll = rollPrice;
 	}
